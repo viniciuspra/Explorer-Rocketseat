5070fd3b171d89382b5994809700ab990752fa00
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-01-10 18:36:11 -0300

    add ex06

---
+++ JavaScript/ex06/script.js
@@ -0,0 +1,25 @@
+
+  let result = prompt("Advinhe o número que estou pensando? Está entre 0 e 10")
+
+  let random = Math.round(Math.random() * 10)
+
+  console.log(random)
+  
+  let tentativas = 1
+
+ if (result == random) {
+  alert("Parabéns! Você adivinhou na 1 tentativa")
+ }
+  while (result != random) {
+      result = prompt("Err0r, Tente Novamente")
+      tentativas++;
+    if (result == random) { 
+      alert(`Parabéns! Você adivinhou o número em ${tentativas} tentativas` )
+    } else if (tentativas > 4) {
+      alert(`Perdeu! Você atingiu o número maximo de tentativas! O número era ${random}`)
+      result = random
+    }
+  }
+
+
+
