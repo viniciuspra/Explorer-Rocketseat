aecd31f0405c58c67355b122f81377b5b53ce18c
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-02-01 19:37:03 -0300

    Update main.js

--- desafios/Focus-Timer/main.js
+++ desafios/Focus-Timer/main.js
@@ -6,41 +6,18 @@ const volumeButton = document.querySelector('.volume');
 const muteButton = document.querySelector('.mute');
 const minutesDisplay = document.querySelector('.minutes');
 const secondsDisplay = document.querySelector('.seconds');
-let minutes
-
-function countdown() {
-    setTimeout(() => {
-      let seconds = Number(secondsDisplay.textContent)
-      let minutes = Number(minutesDisplay.textContent)
-  
-      secondsDisplay.textContent = "00"
-      
-      if (minutes <= 0) {
-        SwitchButtonsControls.SwitchStop()
-        return
-      }
-      
-      if (seconds <= 0) {
-        seconds = 2
-
-        minutesDisplay.textContent = String(minutes - 1).padStart(2, '0')
-      }
-
-      secondsDisplay.textContent = String(seconds - 1).padStart(2, '0').padStart(2, "0")
-      
-      countdown()
-    }, 1000)
-}
+let minutes = Number(minutesDisplay.textContent)
+let TimerTimeOut
 
 const SwitchButtonsControls = {
-  SwitchPlay: function playPause(e) {
+  ResetContols: function playPause(e) {
     pauseButton.classList.toggle("hide")
     playButton.classList.toggle("hide")
     stopButton.classList.remove("hide")
     setButton.classList.add("hide")
     countdown()
   },
-  SwitchPause: function PausePlay() {
+  ResetPlay: function PausePlay() {
     pauseButton.classList.toggle("hide")
     playButton.classList.toggle("hide")
   },
@@ -51,12 +28,43 @@ const SwitchButtonsControls = {
     stopButton.classList.add("hide")
   },
 }
+
+function UpdateTimerDisplay(minutes, seconds) {
+  minutesDisplay.textContent = String(minutes).padStart(2, "0")
+  secondsDisplay.textContent = String(seconds).padStart(2, "0")
+}
+function ResetTimer() {
+  UpdateTimerDisplay(minutes, 0)
+  clearTimeout(TimerTimeOut)
+}
+function countdown() {
+    TimerTimeOut = setTimeout(() => {
+      let seconds = Number(secondsDisplay.textContent)
+      let minutes = Number(minutesDisplay.textContent)
+  
+      UpdateTimerDisplay(minutes, 0)
+      
+      if (minutes <= 0) {
+        SwitchButtonsControls.SwitchStop()
+        return
+      }
+      
+      if (seconds <= 0) {
+        seconds = 60
+        --minutes
+      }
+
+      UpdateTimerDisplay(minutes, String(seconds - 1))
+      
+      countdown()
+    }, 1000)
+}
 function timeFunction() {
   minutes = Number(prompt("Quantos minutos: \n"))
-  if (minutes > 60 || minutes == '' || minutes == 0) {
-    alert("Digite um Numero entre 1 e 60")
+  if (!minutes || minutes >= 100) {
+    SwitchButtonsControls.ResetContols
   }else {
-    minutesDisplay.textContent = String(minutes).padStart(2, '0')
+    UpdateTimerDisplay(minutes, 0)
   }
 }
 function toggleVolume(e) {
@@ -64,9 +72,15 @@ function toggleVolume(e) {
   muteButton.classList.toggle("hide")
 }
 
-playButton.addEventListener("click", SwitchButtonsControls.SwitchPlay)
-pauseButton.addEventListener("click", SwitchButtonsControls.SwitchPause)
+playButton.addEventListener("click", SwitchButtonsControls.ResetContols)
+pauseButton.addEventListener("click", () => {
+  SwitchButtonsControls.ResetPlay()
+  clearTimeout(TimerTimeOut)
+})
 setButton.addEventListener('click', timeFunction);
-stopButton.addEventListener('click', SwitchButtonsControls.SwitchStop);
+stopButton.addEventListener('click', () => {
+  SwitchButtonsControls.SwitchStop()
+  ResetTimer()
+});
 volumeButton.addEventListener('click', toggleVolume)
 muteButton.addEventListener('click', toggleVolume)
