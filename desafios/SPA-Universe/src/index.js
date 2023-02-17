376cefbea7a0424ed68ab36d3226ad546452a98f
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-02-17 11:40:48 -0300

    update SPA

--- desafios/SPA-Universe/src/index.js
+++ desafios/SPA-Universe/src/index.js
@@ -1,14 +1,12 @@
 import { Router } from './Router.js';
 
 const router = new Router()
-router.add(
-  "/desafios/SPA-Universe/index.html",
-  "/desafios/SPA-Universe/pages/home.html"
-)
+router.add("/desafios/SPA-Universe/index.html", "/desafios/SPA-Universe/pages/home.html")
 router.add("/universe", "/desafios/SPA-Universe/pages/universe.html")
 router.add("/exploration", "/desafios/SPA-Universe/pages/exploration.html")
 router.add(404, "/desafios/SPA-Universe/pages/404.html")
+router.add("/more", "/desafios/SPA-Universe/pages/universe.html")
 
-this.handle()
-
+router.handle()
+window.onpopstate = () => router.handle()
 window.route = () => router.route()
